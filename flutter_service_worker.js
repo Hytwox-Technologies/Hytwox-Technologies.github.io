a836663c0f76eccaf584dcff98b610d08f0c655b
Pranav Raut <pranavraut033@gmail.com>
2021-08-26 00:08:15 +0530

    cb0c66e fix: text indent

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {
 "images/logo.svg": "83393b8d05969b429c3c77486d84a29f",
 "index.html": "b8b77ef8528f8cb741348540c0f32b28",
 "/": "b8b77ef8528f8cb741348540c0f32b28",
-"main.dart.js": "df4c1ee6e9b42941611a730d8ca52f88",
+"main.dart.js": "fb59968c4401f4d9fe2f61165e336d3f",
 "manifest.json": "948c1fa6d2373a7318997a23aa4c7d2f",
 "style/index.css": "f4060c2c9c749237a41b08c1b8088cdd",
 "version.json": "06656f6f47c331b4b9ff5023382df168"
